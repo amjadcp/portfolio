35343e645e7a400d94cd13b76c692225c8dbb3d7
amjadcp <amjadcp03@gmail.com>
2025-02-27 22:14:05 +0530

    add: remove main.yml workflow and update HTML content

--- js/main.js
+++ js/main.js
@@ -1,30 +1,34 @@
-let input = document.querySelector("input")
+let input = document.querySelector("input");
 let block = document.getElementById("block");
 
 const inject = async (command) => {
   let data = await fetch(`commands/${command}.html`);
   if (data.status == 404) {
     data = await fetch(`commands/404.html`);
-  }        
+  }
   block.innerHTML += `
-    <br> <span class="command">amjadcp:~$ ${command}</span>
-    <br> ${await data.text()}
+    <br> <br> <span class="command">amjadcp@web:~$ ${command}</span>
+    <br> <br> ${await data.text()} <br> <br>
   `;
 };
 
-window.onload = async () =>{
+window.onload = async (e) => {
+  e.preventDefault();
   await inject("about");
+  await inject("experience");
+  await inject("skill");
+  await inject("connect");
   input.addEventListener("keyup", async function (e) {
     if ((e.keyCode || e.which) == 13) {
       // ENTER key pressed
-      if (input.value == "clear"){
-        block.innerHTML = ""
-      }else if (input.value == "all"){
+      if (input.value == "clear") {
+        block.innerHTML = "";
+      } else if (input.value == "all") {
         await inject("about");
         await inject("experience");
         await inject("skill");
         await inject("connect");
-      }else{
+      } else {
         await inject(input.value);
       }
       input.value = "";
